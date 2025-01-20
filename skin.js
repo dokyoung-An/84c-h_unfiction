f00f46d214a514dcf14e04254e570c61fa400743
Dokyung An <tea-co@naver.com>
2025-01-20 15:45:30 +0900

    수정

--- skin.js
+++ skin.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - Skin
 // Pano2VR 7.0.10/20025
 // Filename: ??? ???? VIORR 84C-ph-??.ggsk
-// Generated 2025-01-20T14:50:25
+// Generated 2025-01-20T15:45:16
 
 function pano2vrSkin(player,base) {
 	player.addVariable('vis_minimap_button', 2, false, { ignoreInState: 0  });
